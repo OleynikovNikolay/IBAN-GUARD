0e3c7e6f593051d9480750d1ec8bfd5328e0b43e
OleynikovNikolay <nikolay.oleynikov96@gmail.com>
2023-11-06 17:52:33 +0100

    changed the error codes

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char* argv[]) {
 
     if (argc < 2)
     {
-        std::cerr << "Usage: " << argv[0] << "--command" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " --command" << std::endl;
         return EXIT_FAILURE;
     }
 
